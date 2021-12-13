017c943d857c13dc0b2ea0a621ba0160e781f3b7
juwiragiye <uwiragiyejanvi@gmail.com>
2021-12-13 04:47:45 -0500

    under-construction

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'Segoe\\ UI',
+          'Roboto',
+          'Helvetica Neue',
+          'Arial',
+          'Noto\\ Sans',
+          'sans-serif',
+          'Apple\\ Color Emoji',
+          'Segoe\\ UI Emoji',
+          'Segoe\\ UI Symbol',
+          'Noto\\ Color Emoji'
+        ]
+      }
+    }
+  },
+  plugins: []
+}
